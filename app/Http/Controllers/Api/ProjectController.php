620f328dd92d2a05f1cec0087840a4e6362e54e9
marcobusacca <marcobusacca01@gmail.com>
2023-08-29 17:26:18 +0200

    [MILESTONE-1] Api Project Controller index

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $projects = Project::all();
+        // $projects = Project::with('type', 'technologies')->get(); EAGER LOADING SENZA PAGINAZIONE
+        // $projects = Project::with('type', 'technologies')->paginate(3); EAGER LOADING CON PAGINAZIONE
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
 }
