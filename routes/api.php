86735735fb0407242d30e5897576abf76fec4380
marcobusacca <marcobusacca01@gmail.com>
2023-08-30 17:26:24 +0200

    [BONUS] Api Type Model

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\ProjectController as ProjectController;
+use App\Http\Controllers\Api\TypeController as TypeController;
 
 /*
 |--------------------------------------------------------------------------
