b885a3396af0f8638e991f43d3f7104809e66b1e
marcobusacca <marcobusacca01@gmail.com>
2023-08-31 15:50:46 +0200

    [FIX]

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,12 +9,11 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index(){
-        
+
         // $projects = Project::all();
         // $projects = Project::with('type', 'technologies')->get(); EAGER LOADING SENZA PAGINAZIONE
-        // $projects = Project::with('type', 'technologies')->paginate(2); EAGER LOADING CON PAGINAZIONE
-
-        $projects = Project::all()->paginate(2);
+        
+        $projects = Project::with('type', 'technologies')->paginate(2); // EAGER LOADING CON PAGINAZIONE
 
         return response()->json([
             'success' => true,
